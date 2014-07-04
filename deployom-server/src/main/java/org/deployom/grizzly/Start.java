24dfb7443a440508b170f3314be2eb05b779856b
Mikhail Volkov <me@mikhailvolkov.com>
2014-07-03 21:13:37 -0400

    STATIC_PATH changed

--- deployom-server/src/main/java/org/deployom/grizzly/Start.java
+++ deployom-server/src/main/java/org/deployom/grizzly/Start.java
@@ -43,7 +43,7 @@ public class Start {
     private static final String JERSEY_CONTEXT = "/jersey";
     private static String SERVER_IP = "0.0.0.0";
     private static Integer SERVER_PORT = 8080;
-    private static String STATIC_PATH = "../deployom-web/web";
+    private static String STATIC_PATH = "../deployom-web";
     private static final Logger logger = Logger.getLogger(Start.class.getName());
 
     public static void main(String[] args) throws IOException, InterruptedException {
